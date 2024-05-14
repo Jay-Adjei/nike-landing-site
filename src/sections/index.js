160aa9ef4b72fb2c0e20f4667eb288db4ab5bc17
Jay-Adjei <jphtadjei@gmail.com>
2024-05-14 21:13:36 +0000

    Hero text

---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import CustomerReviews from "./CustomerReviews";
+import Hero from "./Hero";
+import Nav from "./Nav";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export {
+    CustomerReviews,
+    Hero,
+    Nav,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    Footer
+}
