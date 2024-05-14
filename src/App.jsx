02a07978fda24388bee961f12cce90edcd1ea6fa
Jay-Adjei <jphtadjei@gmail.com>
2024-05-14 10:19:23 +0000

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+const App = () => (
+  <main>
+    <section className="">
+
+    </section>
+    <section className="">
+      
+      </section>
+      <section className="">
+      
+      </section>
+      <section className="">
+      
+      </section>
+      <section className="">
+      
+      </section>
+      <section className="">
+      
+      </section>
+      <section className="">
+      
+      </section>
+  </main>
+);
+
+export default App;
