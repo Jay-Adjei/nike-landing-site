99df2f9f09f7248760cd6d80c0d165ad6f40bffe
Jay-Adjei <jphtadjei@gmail.com>
2024-05-17 20:26:21 +0000

    Super Quality Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,10 +5,11 @@ const Button = ({ label, iconURL }) => {
         bg-coral-red rounded-full text-white border-coral-red">
         {label}
 
-        <img 
+        { iconURL && <img 
             src={iconURL} 
             alt="arrow right icon"
             className="ml-2 rounded-full w-5 h-5"/>
+        }
     </button>
   )
 }
